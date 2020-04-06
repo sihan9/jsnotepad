3dc74a70efe5f50f97c48dc7b9c52291fd3f1cf0
sihan9 <1060192071@qq.com>
2020-04-06 18:48:10 +0800

    记事本代码

---
+++ main.js
@@ -0,0 +1,11 @@
+var font = {
+  fontFamliy:'Agency FB',
+  fontStyle:'',
+  fontWeight:'',
+  fontSize:'8px'
+}
+$(function() {
+  $menubar.show(menuData);
+  var ed = new $editor();
+  ed.show('body');
+});
